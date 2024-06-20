5bd917c971cd8e38fbc3fce5fbc856e968721e21
kevin <kevinlarioshernandez@gmail.com>
2024-06-19 20:28:46 -0600

    commit

--- practica2php/convercion.php
+++ practica2php/convercion.php
@@ -39,7 +39,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Resultado de la Conversión</title>
+    <link rel="stylesheet" href="estilo.css">
 </head>
 <body>
     <h1>Resultado de la Conversión</h1>
